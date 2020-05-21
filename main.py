880b28b43eec2b17b702566672d797614efb1bf7
hotmanparis <58946567+hotmanparis@users.noreply.github.com>
2020-05-21 10:31:51 +0200

    tripletnet2

--- main.py
+++ main.py
@@ -73,6 +73,81 @@ class TripletNetwork(nn.Module):
 
         return l, m, r
 
+class TripletNetwork2(nn.Module):
+    def __init__(self):
+        super(TripletNetwork2, self).__init__()
+        ## encoder layers ##
+        # conv layer (depth from 1 --> 16), 3x3 kernels
+        hdim = 32
+        bdim = 16
+        self.conv1 = nn.Conv2d(3, hdim, 3, padding=1)
+        self.conv2 = nn.Conv2d(hdim, hdim, 3, padding=1)
+        self.conv2_1 = nn.Conv2d(hdim, hdim, 3, padding=1)
+        self.conv3 = nn.Conv2d(hdim, hdim, 3, padding=1)
+        self.conv3_1 = nn.Conv2d(hdim, bdim, 3, padding=1)
+        # conv layer (depth from 16 --> 4), 3x3 kernels
+        self.convB = nn.Conv2d(bdim, 3, 3, padding=1)
+        
+        self.pool = nn.MaxPool2d(2, 2)
+        self.actvn = nn.ReLU()
+        self.dropout = nn.Dropout()
+        
+    def forward(self, x):
+        ## encode ##
+        # add hidden layers with relu activation function
+        # and maxpooling after
+        l = x[:,:,:,:128]
+        m = x[:,:,:,128:256]
+        r = x[:,:,:,256:]
+        
+        l = self.actvn(self.conv1(l))
+        l = self.pool(l)
+        l = self.dropout(l)
+        l = self.actvn(self.conv2(l))
+        l = self.dropout(l)
+        l = self.actvn(self.conv2_1(l))
+        l = self.pool(l)
+        l = self.dropout(l)
+        l = self.actvn(self.conv3(l))
+        l = self.dropout(l)
+        l = self.actvn(self.conv3_1(l))
+        l = self.dropout(l)
+        l = self.convB(l)
+        
+        m = self.actvn(self.conv1(m))
+        m = self.pool(m)
+        m = self.dropout(m)
+        m = self.actvn(self.conv2(m))
+        m = self.dropout(m)
+        m = self.actvn(self.conv2_1(m))
+        m = self.pool(m)
+        m = self.dropout(m)
+        m = self.actvn(self.conv3(m))
+        m = self.dropout(m)
+        m = self.actvn(self.conv3_1(m))
+        m = self.dropout(m)
+        m = self.convB(m)
+        
+        r = self.actvn(self.conv1(r))
+        r = self.pool(r)
+        r = self.dropout(r)
+        r = self.actvn(self.conv2(r))
+        r = self.dropout(r)
+        r = self.actvn(self.conv2_1(r))
+        r = self.pool(r)
+        r = self.dropout(r)
+        r = self.actvn(self.conv3(r))
+        r = self.dropout(r)
+        r = self.actvn(self.conv3_1(r))
+        r = self.dropout(r)
+        r = self.convB(r)
+        
+        l = torch.flatten(l, start_dim=1)
+        m = torch.flatten(m, start_dim=1)
+        r = torch.flatten(r, start_dim=1)
+
+        return l, m, r
+    
 class TripletAlexNet(nn.Module):
     def __init__(self):
         super(TripletAlexNet, self).__init__()
@@ -129,7 +204,8 @@ class TripletAlexNet(nn.Module):
 
 # Initialize model    
 #model = TripletNetwork()
-model = TripletAlexNet()
+model = TripletNetwork2()
+#model = TripletAlexNet()
 model.cuda()
 
 # specify loss function
@@ -159,7 +235,7 @@ for epoch in range(1, n_epochs+1):
         # loss
         l2_plus = torch.mean(torch.square(l-m),dim=1) # size = batch_size,
         l2_min = torch.mean(torch.square(l-r),dim=1) # size = batch_size,
-        loss = torch.mean(F.relu(l2_plus - l2_min + 0.22))
+        loss = torch.mean(F.relu(l2_plus - l2_min + 0.2))
 
         # backward pass: compute gradient of the loss with respect to model parameters
         loss.backward()
@@ -175,7 +251,7 @@ for epoch in range(1, n_epochs+1):
 
         if it%1000 == 0:
             #print('Saving model')
-            torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/alexnet_2.pt")
+            torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/tripletnet2.pt")
           
     # print avg training statistics 
     train_loss = train_loss/len(train_loader)
@@ -185,4 +261,4 @@ for epoch in range(1, n_epochs+1):
         ))
     
     print('Saving model')
-    torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/alexnet_2_epoch.pt")
+    torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/tripletnet2_epoch.pt")
